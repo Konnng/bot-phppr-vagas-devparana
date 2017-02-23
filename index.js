9708925836b52f9f6c3940f355efef6cd232bfeb
Konnng <falecom@konnng.com>
2017-02-23 00:41:51 -0300

    refactoring and use of Q promises

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var GitHubApi = require('github-api-node')
-var fs = require('fs-extra')
-var path = require('path')
-var Slack = require('slack-node')
-var moment = require('moment')
-var lowDb = require('lowdb')
-var lowDbFileAsync = require('lowdb/lib/storages/file-async')
-var sleep = require('system-sleep')
+let GitHubApi = require('github-api-node')
+let fs = require('fs-extra')
+let path = require('path')
+let Slack = require('slack-node')
+let moment = require('moment')
+let lowDb = require('lowdb')
+let lowDbStorage = require('lowdb/lib/storages/file-sync')
+let sleep = require('system-sleep')
+let Promise = require('q')
 
-var slackWebHook = process.env.LABS_SLACK_WEBHOOK_URL_DEVPARANA_BOT_PR || ''
-var dbFile = path.join(__dirname, 'data/db.json')
+let slackWebHook = process.env.LABS_SLACK_WEBHOOK_URL_DEVPARANA_BOT_PR || ''
+let dbFile = path.join(__dirname, 'data/db.json')
 
 try {
   if (!fs.existsSync(path.dirname(dbFile)) && !fs.mkdirsSync(path.dirname(dbFile))) {
@@ -17,64 +18,77 @@ try {
     throw new Error('Slack Webhook not found in enviroment variables. Aborting...')
   }
 
-  var db = lowDb(dbFile, { storage: lowDbFileAsync, writeOnChange: true })
-  var github = new GitHubApi({})
-  var slack = new Slack()
+  let db = lowDb(dbFile, { storage: lowDbStorage })
+  let github = new GitHubApi({})
+  let slack = new Slack()
+  let deferred = Promise.defer()
   slack.setWebhook(slackWebHook)
 
   db.defaults({ jobs: [], settings: {} }).write()
 
-  var jobs = db.get('jobs').value().map(item => item.id)
-  var issues = github.getIssues('phppr', 'vagas')
+  let savedJobs = db.get('jobs').map('id').value() || []
+  let issues = github.getIssues('phppr', 'vagas')
 
   issues.list({}, (err, data) => {
     if (err) {
       throw err
     }
 
-    data.filter(item => jobs.indexOf(item.id) < 0).forEach(item => {
-      var id = item.id
-      var title = item.title
-      var url = item.html_url
-      var labels = item.labels.map(label => label.name)
-      var date = moment(item.created_at).unix()
-      var dateProcessed = moment().unix()
-      var botProcessed = false
+    data.filter(item => savedJobs.indexOf(item.id) < 0).forEach(item => {
+      let id = item.id
+      let title = item.title
+      let url = item.html_url
+      let labels = item.labels.map(label => label.name)
+      let date = moment(item.created_at).unix()
+      let dateProcessed = moment().unix()
+      let botProcessed = false
 
-      var row = { id, title, url, labels, date, dateProcessed, botProcessed }
+      let row = { id, title, url, labels, date, dateProcessed, botProcessed }
 
       db.get('jobs').push(row).write()
+      sleep(100)
     })
+
+    deferred.resolve()
   })
 
-  jobs = db.get('jobs').value().filter(item => !item.botProcessed).map((item, index) => {
-    slack.webhook({
-      attachments: [
-        {
+  Promise.when(deferred).then(() => {
+    let jobs = db.get('jobs').value().filter(item => !item.botProcessed)
+      .filter((item, index, self) => index === self.findIndex(_item => item.id === _item.id))
+
+    _log(`Found ${jobs.length} job offers.`)
+    if (jobs.length) {
+      _log('Processing items to send to slack...')
+    }
+
+    jobs.forEach((item, index) => {
+      _log('Processing item ' + (index + 1))
+      slack.webhook({
+        attachments: [{
           title: item.title,
           title_link: item.url,
           text: 'Vaga: ' + item.title + '\nData: ' + moment(item.date).format('DD/MM/YYYY') + '\nDetalhes: ' + item.labels.join(', '),
           color: '#7CD197'
+        }],
+        text: 'Vaga de trabalho encontrada. Confira! \n\n' + item.url
+      }, (err, response) => {
+        if (err) {
+          throw err
         }
-      ],
-      text: 'Vaga de trabalho encontrada. Confira! \n\n' + item.url
-    }, (err, response) => {
-      if (err) {
-        throw err
-      }
-      if (response.statusCode === 200) {
-        _log('Done posting item ' + (index + 1))
-        db.get('jobs').find({ id: item.id }).assign({ bot_processed: true }).write()
-      } else {
-        throw new Error('Error processing item ' + (index + 1) + ': ' + response.statusCode + ': ' + response.statusMessage)
-      }
+        if (response.statusCode === 200) {
+          _log('Done posting item ' + (index + 1))
+          _log('-'.repeat(100))
+          db.get('jobs').find({ id: item.id }).assign({ botProcessed: true }).write()
+        } else {
+          throw new Error('Error processing item ' + (index + 1) + ': ' + response.statusCode + ': ' + response.statusMessage)
+        }
+      })
+      sleep(1000)
     })
-
-    sleep(1000)
   })
 } catch (err) {
   _log('ERROR: ', err)
-  _log('ERROR: ', '-'.repeat(100))
+  _log('-'.repeat(100))
 }
 
 function _log () {
